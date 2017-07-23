b05abb0e585cabf72f36078f026062534d4f0930
Alistair Lynn <arplynn@gmail.com>
2017-07-23 18:48:29 +0100

    IT WORKS

--- inflate.py
+++ inflate.py
@@ -1,96 +1,48 @@
 from PIL import Image
 from lxml import etree
-from shapely.geometry import Polygon
-from shapely.ops import cascaded_union
+
+import pyclipper
 
 import random
 import itertools
 
-REGION_SHOW = True
-
-def tp_contiguous(polygon, touching_points):
-    points = list(polygon.exterior.coords)[:-1]
-
-    touching_point_indices = [
-        points.index(x)
-        for x in touching_points
-    ]
-    touching_point_indices.sort()
-
-    return any(
-        touching_point_indices[x + 1] == (
-            touching_point_indices[x] + 1
-        ) % len(points)
-        for x in range(len(touching_point_indices) - 1)
-    )
-
-def merge_polygons(polygons):
-    working_queue = list(polygons)
-
-    while working_queue:
-        focus = working_queue.pop()
-
-        new_working_queue = []
-
-        for candidate in working_queue:
-            focus_points = set(focus.exterior.coords)
-            candidate_points = set(candidate.exterior.coords)
-            touching_points = focus_points & candidate_points
-            if (
-                len(touching_points) >= 2 and
-                tp_contiguous(focus, touching_points) and
-                tp_contiguous(candidate, touching_points)
-            ):
-                # Narrow-phase: make sure the points are contiguous
-                focus_prime = focus.union(candidate)
-
-                if isinstance(focus_prime, Polygon):
-                    focus = focus_prime
-                    continue
-            new_working_queue.append(candidate)
-
-        working_queue = new_working_queue
-
-        yield focus
+REGION_SHOW = False
 
 def paths(accessor, width, height):
-    # Construct initial polygons
-    polygons = [
-        Polygon([
-            (x, y),
-            (x + 1, y),
-            (x + 1, y + 1),
-            (x, y + 1),
-            (x, y),
-        ]).convex_hull
-        for y in range(height)
-        for x in range(width)
-        if accessor[x, y]
-    ]
+    pc = pyclipper.Pyclipper()
 
-    num_polygons = len(polygons)
+    for y in range(height):
+        for x in range(width):
+            if not accessor[x, y]:
+                continue
 
-    for iteration in range(3):
-        print(f"Iteration {iteration}: {num_polygons} polys")
-        polygons = list(merge_polygons(polygons))
-        new_num_polygons = len(polygons)
+            pc.AddPath([
+                (x, y),
+                (x + 1, y),
+                (x + 1, y + 1),
+                (x, y + 1),
+            ], pyclipper.PT_SUBJECT, True)
 
-        if new_num_polygons == num_polygons:
-            break
+    result = pc.Execute2(
+        pyclipper.CT_UNION,
+        pyclipper.PFT_EVENODD,
+        pyclipper.PFT_EVENODD,
+    )
 
-        num_polygons = new_num_polygons
+    print(result.Parent)
 
-    return [
-        list(x.simplify(0.01).exterior.coords)
-        for x in polygons
-    ]
+    def descend_node(node):
+        if node.Contour:
+            yield node.IsHole, node.Contour + [node.Contour[0]]
+        for child in node.Childs[::-1]:
+            yield from descend_node(child)
+
+    yield from descend_node(result)
 
 def convert(path, fp, scale_factor=100):
     img = Image.open(str(path))
     img = img.convert('1')
-    #img = img.point(lambda x: 0 if x else 1)
-    #img = img.point(lambda x: 255 if x else 0)
-    #img = img.point(lambda x: 255 if x < 128 else 0)
+    img = img.point(lambda x: 0 if x else 1)
     unit_paths = paths(img.load(), img.size[0], img.size[1])
 
     svg_namespace = 'http://www.w3.org/2000/svg'
@@ -128,6 +80,7 @@ def convert(path, fp, scale_factor=100):
                     tag('transform', svg_namespace):
                         f'translate({margin} {margin}) scale({scale_factor})',
                     tag('stroke', svg_namespace): 'none',
+                    tag('fill-rule', svg_namespace): 'evenodd',
                 }
             ):
                 if REGION_SHOW:
@@ -156,7 +109,7 @@ def convert(path, fp, scale_factor=100):
                 else:
                     region_colours = itertools.cycle(['black'])
 
-                for path in unit_paths:
+                for is_hole, path in unit_paths:
                     path_components = [f"M{path[0][0]} {path[0][1]}"]
 
                     print(path)
@@ -167,7 +120,10 @@ def convert(path, fp, scale_factor=100):
                     )
 
                     path_components.append("z")
-                    colour = next(region_colours)
+                    if is_hole:
+                        colour = 'white'
+                    else:
+                        colour = next(region_colours)
 
                     with f.element(
                         tag('path', svg_namespace),
@@ -179,4 +135,4 @@ def convert(path, fp, scale_factor=100):
                         pass
 
 with open('output.svg', 'wb') as f:
-    convert('/Users/alynn/Downloads/tag36h11/tag36_11_00001.png', f)
+    convert('/Users/alynn/Downloads/tag36h11/tag36_11_00003.png', f)
